c052abdbddf73b3f90cfcd2974e9dcac842ed71b
S Chopra <sanjaybenu@gmail.com>
2023-08-08 17:14:37 +1000

    added react app and initialised npm

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header/header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      I am the App page
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./style.css"
+
+const Header = ()=>{
+
+    return (
+<h1>I am a Header</h1>
+    )
+}
+
+export default Header
